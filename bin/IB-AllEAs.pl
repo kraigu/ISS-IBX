b141f81abec0fe97d9f31b34b7deed0cc2078616
Mike Patterson <mike@snowcrash.ca>
2013-04-29 11:08:37 -0400

    working version of AllEAs

--- bin/IB-AllEAs.pl
+++ bin/IB-AllEAs.pl
@@ -7,16 +7,17 @@
 
 use strict;
 use warnings;
+use IO::Socket::SSL;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use Config::General;
 use Data::Dumper;
-use Infoblox;
+use Infoblox qw ( :hostaddress );
 use LWP::UserAgent;
 use HTTP::Request;
 use Net::IPv4Addr qw( :all );
 use ISSIBX;
-use vars qw/$opt_f $opt_v $opt_h/;
+use vars qw/$opt_f $opt_v $opt_h $opt_f/;
 use Getopt::Std;
 
 my %config;
@@ -26,12 +27,15 @@ if($opt_h){
     exit 0;
 }
 my $debug = $opt_v || 0;
+if($debug){ print "DEBUG $debug\n"; }
 if($opt_f){
 	%config = ISSIBX::GetConfig($opt_f);
+	if($debug > 1) {
+		print "DEBUG using configuration file $opt_f\n";
+	}
 } else {
 	%config = ISSIBX::GetConfig();
 }
-if($debug){ print "DEBUG $debug\n"; }
 
 sub hostpprint() {
 	if($debug > 2){
@@ -39,7 +43,7 @@ sub hostpprint() {
 		print Dumper($_[0]);
 	}
 	my $obj_host = $_[0];
-	my $v4address;
+	my ($v4address,$top);
 	my @eas;
 
 	# Skip disabled hosts
@@ -49,17 +53,23 @@ sub hostpprint() {
 	}
 
 	# array reference that might contain Infoblox::DHCP::FixedAddr objects
-	# or it might contain a 
 	return unless defined($obj_host->ipv4addrs());
 	my @ipv4addrs = @{ $obj_host->ipv4addrs() };
-	$v4address = ($ipv4addrs[0]); #->ipv4addr());
 
-	# Now extract the name, the FQDN and the hostname from the data
+	$v4address = ($ipv4addrs[0]->ipv4addr());
+
 	my $name = ($obj_host->name());
-	@eas = ($obj_host->extensible_attributes());
-	if (defined($eas[0]{"Pol8-Classification"} ) ){
-		print qq|$name\t$v4address\t$eas[0]{"Pol8-Classification"}\n|;
+	if($debug > 3) {
+		print "DEBUG dumping ipv4addrs for $name\n" . Dumper(@ipv4addrs) . "\n";
 	}
+	@eas = ($obj_host->extensible_attributes());
+	$top = qq|"$name","$v4address",|;
+	if (defined($eas[0]{"Pol8 Classification"} ) ){
+		$top = $top . qq|"$eas[0]{"Pol8 Classification"}"|;
+	} else {
+		$top = $top . qq|""|;
+	}
+	print "$top\n";
 }
 
 sub getHosts() {
@@ -83,7 +93,6 @@ sub getHosts() {
 }
 
 ## main routine
-
 if($opt_f){
 	%config = ISSIBX::GetConfig($opt_f);
 } else {
@@ -92,13 +101,11 @@ if($opt_f){
 
 # Verify that the remote is responding. This may not be strictly necessary.
 my $timeout = 10;
-my $ua = LWP::UserAgent->new(
-	useragent => [
-		ssl_opts => {
-			SSL_verify_mode => 'SSL_VERIFY_NONE' # this doesn't seem to work? :~(
-		}
-	]
-);
+my $ua = LWP::UserAgent->new();
+$ua->ssl_opts( verify_hostname => 0); # security guys still hating security
+if($debug > 2){
+	print "DEBUG Dumping ua\n" . Dumper($ua) . "\n";
+}
 my $request = HTTP::Request->new(GET => "https://$config{hostname}");
 eval {
 	local $SIG{ALRM} = sub { die "timeout exceeded\n" };
@@ -139,3 +146,10 @@ if ( my $herp = $ibsession->status_code() ){
 
 # Do the actual work
 &getHosts($ibsession);
+
+# OK, so, yeah, we hate security and stuff.
+BEGIN {
+    IO::Socket::SSL::set_defaults(
+        verify_mode => Net::SSLeay->VERIFY_NONE(),
+    );
+}
